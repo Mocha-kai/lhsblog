523aa5459fa246675ee2b9deb82d70558eb8a2cb
Mocha-kai <kai@mocha-company.com>
2023-08-04 17:51:16 +0900

    T

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,24 +1,24 @@
-import styles from './ProfileCard.module.css';
-import myPicture from './../../../public/profilePicture/lhs_0.jpg';
+import Styles from './ProfileCard.module.css';
+import Image from 'next/image';
+import MyPicture from './../../../public/profilePicture/lhs_0.jpg';
 import Java from './../../../public/icon/icons8-java.gif';
 import Js from './../../../public/icon/icons8-js.gif';
 import React from './../../../public/icon/icons8-react.gif';
 import Github from './../../../public/icon/icons8-github.gif';
-import Image from 'next/image';
 
 const ProfilePage = () => {
     return (
-        <div className={styles[`profile-main`]}>
-            <div className={styles['profile-card']}>
-                <Image src={myPicture} alt="Profile" className={styles['profile-image']} />
-                <div className={styles['profile-name']}>Lhs</div>
-                <div className={styles['profile-occupation']}>Web Developer</div>
-                <div className={styles['profile-occupation']}>1992. 12</div>
+        <div className={Styles[`profile-main`]}>
+            <div className={Styles['profile-card']}>
+                <Image src={MyPicture} alt="Profile" className={Styles['profile-image']} />
+                <div className={Styles['profile-name']}>Lhs</div>
+                <div className={Styles['profile-occupation']}>Web Developer</div>
+                <div className={Styles['profile-occupation']}>1992. 12</div>
                 <div>
-                    <Image src={Java} alt="Profile" className={styles['icon-image']} />
-                    <Image src={Js} alt="Profile" className={styles['icon-image']} />
-                    <Image src={React} alt="Profile" className={styles['icon-image']} />
-                    <Image src={Github} alt="Profile" className={styles['icon-image']} />
+                    <Image src={Java} alt="Profile" className={Styles['icon-image']} />
+                    <Image src={Js} alt="Profile" className={Styles['icon-image']} />
+                    <Image src={React} alt="Profile" className={Styles['icon-image']} />
+                    <Image src={Github} alt="Profile" className={Styles['icon-image']} />
                 </div>
             </div>
         </div>
